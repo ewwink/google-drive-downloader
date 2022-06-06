afcc6ef0ab799476f8ba255d4fe0ea1781097a38
erwin solihin <ewwink@users.noreply.github.com>
2022-06-06 23:04:23 +0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,10 +2,10 @@ from setuptools import setup
 
 setup(
     name='googledrivedownloader',
-    version='0.4',
+    version='0.4.1',
     packages=['google_drive_downloader'],
-    url='https://github.com/ndrplz/google-drive-downloader',
-    download_url='https://github.com/ndrplz/google-drive-downloader/archive/0.2.tar.gz',
+    url='https://github.com/ewwink/google-drive-downloader',
+    download_url='https://github.com/ewwink/google-drive-downloader/archive/0.2.tar.gz',
     license='MIT',
     author='Davide Abati and Andrea Palazzi',
     author_email='ndrplz@gmail.com',
